0bd66f2e9ce7a9db842ee888f76760c9a174f2a9
EmRodDev <erodriguezpersonalmail@gmail.com>
2024-02-20 05:35:24 -0300

    Added more routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,24 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController.js');
 const usersController = require('../controllers/usersController.js');
+const authController = require('../controllers/authController.js');
+const adminController = require('../controllers/adminController.js');
+
 
 module.exports = function (){
     router.get('/', homeController.home);
 
-    router.get('/create-account', usersController.createAccountForm);
+    /** Create and confirm accounts **/
+    router.get('/sign-up', usersController.createAccountForm);
+    router.post('/sign-up', usersController.createNewAccount);
+    router.get('/confirm-account/:email', usersController.confirmAccount);
+
+    //Sign in
+    router.get('/sign-in', usersController.signInForm);
+    router.post('/sign-in', authController.authenticateUser);
+
+    //Administration panel
+    router.get('/admin', adminController.adminPanel);
 
     return router
 }
