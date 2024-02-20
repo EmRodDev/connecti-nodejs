75502154fc4bfb17e844f00c94b5559b5f6414da
EmRodDev <erodriguezpersonalmail@gmail.com>
2024-02-20 05:33:05 -0300

    Body parser, Cookie parser, Session, Passpor and Flash configurations

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
 const express = require('express');
 const path = require('path');
 const router = require('./routes');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 const expressEjsLayouts = require('express-ejs-layouts');
+const passport = require('./config/passport.js');
 
+//DB configuration and models
 const db = require('./config/db.js');
 require('./models/Users.js');
 db.sync().then(() => console.log('Connected to DB')).catch(err => console.error(err));
 
+//Environment variables
 require('dotenv').config({path: '.env'});
 
+//Main application
 const app = express();
 
-//Enable EJS as template engine
+//Body parser, read forms
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+//Express validator (Validation with a lot of functions)
+app.use(expressValidator());
+
+//Enable EJS as template engine
 app.use(expressEjsLayouts);
 app.set('view engine', 'ejs');
 
 //Static files
-
 app.use(express.static('public'));
+
+//Enable cookie parser
+app.use(cookieParser());
+
+//Create the session
+app.use(session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false
+}));
+
+//Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Add flash messages
+app.use(flash());
 
 //Middleware (Logged user, flash messages, actual date)
 app.use((req, res, next) => {
+    res.locals.messages = req.flash();
     const date = new Date();
     res.locals.year = date.getFullYear();
     next();
