f0869d2c6ef0ade36b2b30f667a76b20525da410
EmRodDev <erodriguezpersonalmail@gmail.com>
2024-03-14 05:43:31 -0300

    Added the functions of profile edit and image customization

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,58 @@
 const Users = require('../models/Users.js');
 const sendEmail = require('../handlers/email.js');
 
+const multer = require('multer');
+const shortid = require('shortid');
+const fs = require('fs');
+const maxSize = 2 * 1000 * 1000;
+
+const multerConfig = {
+    limits: { fileSize: maxSize},
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, file, next) => {
+            next(null, __dirname+'/../public/uploads/profiles/')
+        },
+        filename: (req, file, next) => {
+            const extension = file.mimetype.split('/')[1];
+            next(null, `${shortid.generate()}.${extension}`);
+        }
+    }),
+    fileFilter(req, file, next){
+        if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+            //The format is valid
+            next(null,true);
+        }else{
+            //The format is invalid
+            next(new Error('File format not valid'), false);
+        }
+    }
+}
+
+const upload = multer(multerConfig).single('image');
+
+//Upload an image on the server
+exports.uploadImage = (req, res, next) => {
+    upload(req,res, function(err){
+        if(err){
+            //Handle errors
+            if(err instanceof multer.MulterError){
+                if(err.code === 'LIMIT_FILE_SIZE'){
+                    req.flash('error','The image file is too large');
+                }else{
+                    req.flash('error', err.message);
+                }
+            } else if (err.hasOwnProperty('message')){
+                req.flash('error', err.message);
+            }
+            return res.redirect('back');
+            
+        }else {
+            next();
+        }
+    })
+};
+
+
 exports.createAccountForm = (req,res) => {
     res.render('create-account', {
         namePage: 'Create account'
@@ -94,3 +146,110 @@ exports.signInForm = (req,res) => {
         namePage: 'Sign in'
     });
 };
+
+//Show a form to edit the profile
+exports.editProfileForm = async (req, res) => {
+    const user = await Users.findByPk(req.user.id);
+
+    res.render('edit-profile', {
+        namePage: 'Edit Profile',
+        user
+    })
+}
+
+//Store the changes on the DB
+exports.editProfile = async (req, res) => {
+    const user = await Users.findByPk(req.user.id);
+
+    //Sanitize form
+    req.sanitizeBody('name');
+    req.sanitizeBody('email');
+
+    //Read form data
+
+    const {name, description, email} = req.body;
+
+    //Assign the values
+
+    user.name = name;
+    user.description = description;
+    user.email = email;
+
+    //Store changes
+    await user.save();
+    req.flash('success','Changes saved succesfully');
+    res.redirect('/admin');
+}
+
+//Show a form to modify the password
+
+exports.changePasswordForm = (req, res) => {
+    res.render('change-password', {
+        namePage: 'Change Password'
+    })
+}
+
+//Check if the previous password is valid and replace it for a new one
+exports.changePassword = async (req, res, next) => {
+    const user = await Users.findByPk(req.user.id);
+
+    //Verify that the previous password is valid
+    if(!user.validatePassword(req.body.previous)){
+        req.flash('error', 'The provided actual passowrd is incorrect');
+        res.redirect('/change-password');
+        return next();
+    }
+
+    //If the password is valid, hash the new one
+    const hash = user.hashPassword(req.body.new);
+
+    //Assign the password to the user
+    user.password = hash;
+
+    //Store on the DB
+    await user.save();
+
+    //Redirect
+    req.flash('success','Password Modified Successfully');
+    res.redirect('/admin');
+}
+
+//Show the profile image upload form
+exports.uploadImageForm = async (req, res) => {
+    const user = await Users.findByPk(req.user.id);
+
+    //Show the view
+    res.render('img-profile', {
+        namePage: 'Upload or Update Profile Image',
+        user
+    });
+
+
+}
+
+//Save the new image, delete the previous one if corresponds
+exports.saveProfileImage = async (req, res) => {
+    const user = await Users.findByPk(req.user.id);
+
+    //If there's a previous image, delete it
+    if(req.file && user.image){
+        const previousImgPath = __dirname + `/../public/uploads/profiles/${user.image}`;
+        
+        //Delete file
+        fs.unlink(previousImgPath, (err) => {
+            if(err){
+                console.error(err);
+            }
+        });
+    }
+
+    //Store the new image
+    if(req.file){
+        user.image = req.file.filename;
+    }
+
+    //Store on the DB and redirect
+    await user.save();
+    req.flash('success','Changes saved successfully');
+    res.redirect('/admin');
+}
